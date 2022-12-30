584b63619e43680902f3a81438d48b0547112869
shiwani <shiwanipaunikar1@gmail.com>
2022-12-29 22:45:30 -0800

    [shiwani]analyse and respond happy and sad mood program is added

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyzer.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyzer {
+    private String message;
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if(message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+
+import com.bridgelabz.moodanalyser.MoodAnalyzer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    public void givenMessgae_WhenSad_ShouldReturn_Sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad Message");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD",mood);
+
+    }
+
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is Happy Messgae");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
+
+}
