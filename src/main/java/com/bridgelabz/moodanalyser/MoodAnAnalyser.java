aeffb6371720468fa7f29895610fc54adfbdf72d
shiwani <shiwanipaunikar1@gmail.com>
2022-12-29 22:32:39 -0800

    [shiwani]mood analyser program

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to mood analyser program");
+    }
+}
